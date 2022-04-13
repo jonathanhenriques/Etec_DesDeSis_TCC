7229ff9cc471141763a584beb39f8d5d4989fa03
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-04-12 22:15:29 -0300

    Adicionando Página 2

---
+++ carousel.php
@@ -0,0 +1,24 @@
+
+
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.diariodopeixe.com.br/wp-content/uploads/2020/12/pele-2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.santosfc.com.br/wp-content/uploads/2020/10/Pel%C3%A9-Propostas.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTJS8Hl-XOiYqNrgdG7e1eEipLMMY2v3aIt-ksCdkCjyO0c6-SUBMZbM02x37n-JxXYc8&usqp=CAU" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
--- pagina.php
+++ pagina.php
@@ -12,41 +12,7 @@
 
     <header>
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="teste2.php">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Historia <span class="sr-only">(página atual)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Troféus</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="teste2.php">Apoie</a>
-                <a class="dropdown-item" href="#">Outra ação</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Algo mais aqui</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Desativado</a>
-            </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-            </form>
-        </div>
-        </nav>
+    <?php require_once "nav.php" ?>
 
     </header>
 
@@ -65,44 +31,9 @@
     </div>
 
 
-    <div id="demo" class="carousel slide" data-ride="carousel">
+    <?php require_once "carousel.php" ?>
 
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-
-  <!-- The slideshow -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://jpimg.com.br/uploads/2020/10/pele326.181402-1024x850.jpg" alt="Los Angeles">
-    </div>
-    <div class="carousel-item">
-      <img src="https://amigosdepelotas.com.br/wp-content/uploads/2021/12/5b9a62546be90988a01171738aced282.jpg" alt="Chicago">
-    </div>
-    <div class="carousel-item">
-      <img src="https://amigosdepelotas.com.br/wp-content/uploads/2021/12/5b9a62546be90988a01171738aced282.jpg" alt="New York">
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-
-</div>
-
-
-    <figure class="figure">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Santos_logo.svg/1200px-Santos_logo.svg.png" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-        <figcaption style="text-align: center;" class="figure-caption">Maior Time de todos!.</figcaption>
-    </figure>
-    </div>
+    
 
    <?php require_once "administracao.php" ?>
 
