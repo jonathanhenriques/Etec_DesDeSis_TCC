75ed260b32a0ac453122b77756d23e7b9777eec8
Jonathan <silva.henriquejonas@gmail.com>
2022-04-26 22:08:47 -0300

    Rascunho no PHP

--- pagina.php
+++ pagina.php
@@ -1,3 +1,4 @@
+
 <?php include "funcao.php"?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,37 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php  require_once "bootstrap.php" ?>
-    <title>Document</title>
+    <title>TCC</title>
 </head>
 <body>
 
     <header>
 
-    <?php require_once "nav.php" ?>
-
-    </header>
-
-    <div style="width: 100vw; heigth: 100vh; display: flex; flex-direction: row;justify-content: center; align-items: center;">
-        <?php include "formu.php" ?>
-    </div>
-
-    <div class="container">
-    <h1>TESTE!!!!!!!</h1>
-    <?php mensagem("Maior de todos"); ?>
-
-    <div class="btn-group" role="group" aria-label="Exemplo básico">
-        <button type="button" class="btn btn-secondary">Esquerda</button>
-        <button type="button" class="btn btn-secondary">Meio</button>
-        <button type="button" class="btn btn-secondary">Direita</button>
-    </div>
-
-
-    <?php require_once "carousel.php" ?>
-
+    <br><br><br><br><br><br><br><br>
+        
+    <?php require_once "rascunhoForm.php"?>
     
-
-   <?php require_once "administracao.php" ?>
-
 
 </body>
 </html>
